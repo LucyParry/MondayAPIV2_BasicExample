5d9efd295036ef57a43e84b183ccbf60115f85a4
Lucy <lucy.parry@gmail.com>
2019-08-14 19:47:20 +0100

    Add project files.

---
+++ MondayAPIV2_BasicExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MondayAPIV2_BasicExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
