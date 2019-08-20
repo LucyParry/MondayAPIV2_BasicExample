ebcf1afea63d8160b83e076e11d36e98d0c177d5
Lucy <lucy.parry@gmail.com>
2019-08-20 09:40:47 +0100

    Add using statement with explanation

--- MondayAPIV2_BasicExample/Program.cs
+++ MondayAPIV2_BasicExample/Program.cs
@@ -1,4 +1,4 @@
-﻿using MondayV2API_BasicExample.MondayEntities;
+using MondayV2API_BasicExample.MondayEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,23 +11,28 @@ namespace MondayAPIV2_BasicExample
         {
             string apiToken = "YOUR TOKEN HERE";
             string apiRoot = "https://api.monday.com/v2/";
-
-            var client = new MondayClient(apiToken, apiRoot);
-            var service = new MondayService(client);
-
-            // get all boards
-            List<Board> boards = service.GetBoards();
-            boards.ForEach(x => Console.WriteLine(x.Name));
-
-            if (boards.Any())
+            
+            // using statement used here to dispose of the client, but it's recommended to reuse HttpClient as much as possible in a real situation
+            // see https://blogs.msdn.microsoft.com/shacorn/2016/10/21/best-practices-for-using-httpclient-on-services/
+            using (var client = new MondayClient(apiToken, apiRoot)) 
             {
-                // get items for the first board in the list
-                Board board = service.GetBoardWithItems(boards[0].Id);
-                foreach (var boardItem in board.Items)
+                var service = new MondayService(client);
+
+                // get all boards
+                List<Board> boards = service.GetBoards();
+                boards.ForEach(x => Console.WriteLine(x.Name));
+
+                if (boards.Any())
                 {
-                    Console.WriteLine(boardItem.Name);
+                    // get items for the first board in the list
+                    Board board = service.GetBoardWithItems(boards[0].Id);
+                    foreach (var boardItem in board.Items)
+                    {
+                        Console.WriteLine(boardItem.Name);
+                    }
                 }
             }
+                        
             Console.Read();
         }
     }
